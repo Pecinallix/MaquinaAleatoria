f2af5b04eeda501795c6ed197e62f4fbc3e74b5a
Icaro Pecinalli <icaropecinalli@hotmail.com>
2023-01-07 17:28:15 -0300

    começando a M.A

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var imagens = document.querySelector(img);
+const nome = 'Icaro';
+
+console.log(nome);
+console.log(imagens);
+
+var numeroescolhido = Math.round(Math.random() * 10);
+console.log(numeroescolhido);
